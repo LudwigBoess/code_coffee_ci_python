1aae9c9df3b0f7ff943a6d59bd82535ac352ecc0
LudwigBoess <lboess@usm.lmu.de>
2020-11-29 15:43:38 +0100

    initial upload

---
+++ tests/test_arithmetic.py
@@ -0,0 +1,19 @@
+from src.arithmetic import Arithmetic
+
+
+def test_addition():
+    arith = Arithmetic(6,4)
+    assert arith.addition() == 10
+
+def test_subtraction():
+    arith = Arithmetic(6,4)
+    assert arith.subtraction()== 2
+
+def test_multiplication():
+    arith = Arithmetic(6,4)
+    assert arith.multiplication() == 24
+
+def test_division():
+    arith = Arithmetic(6,4)
+    assert arith.division() == 1.5
+
